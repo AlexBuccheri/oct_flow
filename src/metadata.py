640fad197d7d7df9669397cbc237c6636e3aeeea
Alex Buccheri <alexanderbuccheri@googlemail.com>
2023-10-27 20:31:15 +0100

    Functions from prior project.

---
+++ src/metadata.py
@@ -0,0 +1,54 @@
+""" Metadata
+"""
+import hashlib
+from typing import List
+
+
+def create_hash(input_string: str):
+    """ Generate a unique hash from any input file string.
+
+    :param input_string:
+    :return:
+    """
+    # Strip all whitespace and terminating characters from the string
+    stripped_input = input_string.replace('\t', '').replace('\n', '').replace(' ', '')
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(stripped_input.encode('utf-8'))
+    # hexadecimal representation of the hash value
+    hashed_string = sha256_hash.hexdigest()
+    return hashed_string
+
+
+def generate_config_data(options_dicts: List[dict], inputs: List[str]) -> dict:
+    """
+
+    Generate a file index and also a set of unique settings, and a hash.
+    This will a) make it easier when parsing data for plotting and
+    b) avoid having to create some directory naming convention based on the
+    unique input options, like:
+
+    eig-rmm_eig_1e-7_file-1AL
+
+    1:
+      -  Eigensolver: rmmdiis
+      -  EigensolverTolerance: 1e-7
+      -  File: 1ALA
+      -  input_hash: ojj9uotrreji
+    2:
+      -  Eigensolver: rmmdiis
+      -  EigensolverTolerance: 1e-7
+      -  File: 2ALA
+      -  input_hash: 3u84jfio4390
+    ...
+
+    :param options_dicts:
+    :return:
+    """
+    assert len(options_dicts) == len(inputs)
+    config = {}
+
+    for i, options in enumerate(options_dicts):
+        config[i] = options
+        config[i].update({'input_hash': create_hash(inputs[i])})
+
+    return config
---
+++ src/utils.py
@@ -0,0 +1,26 @@
+import itertools
+
+
+def cartesian_product(matrix: dict):
+    """ Create all combinations of options, where the dict
+    keys define the options and the values define the
+    specific values that the optionals should take.
+
+    Expects a dict of the form:
+
+    {'Eigensolver': ['rmmdiis', 'chebyshev_filter'],
+     'EigensolverTolerance':'1e-7'}
+
+    with no nesting of containers in values.
+
+    :param matrix:
+    :return:
+    """
+    # Get all possible combinations
+    combinations = list(itertools.product(*matrix.values()))
+
+    # Create a list of dictionaries by pairing each key with its corresponding value from the combinations
+    all_option_permutations = [{key: value for key, value in zip(matrix.keys(), combination)} for combination in
+                               combinations]
+
+    return all_option_permutations
