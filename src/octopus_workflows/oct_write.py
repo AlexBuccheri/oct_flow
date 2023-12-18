ee223324da14f33d2df30ed3313e5608a689c8bf
Alex Buccheri <alexander.buccheri@mpsd.mpg.de>
2023-12-18 14:55:39 +0000

    Move write_extended_xyz to correct file

--- src/octopus_workflows/oct_write.py
+++ src/octopus_workflows/oct_write.py
@@ -1,5 +1,7 @@
 """ Convert dictionary to octopus input file string.
 """
+import ase
+import numpy as np
 
 
 def write_octopus_input(options: dict) -> str:
@@ -25,3 +27,38 @@ def write_octopus_input(options: dict) -> str:
             input += f"{key} = {value}\n"
 
     return input
+
+
+def write_extended_xyz(xyz_file, atoms: ase.atoms.Atoms):
+    """
+    NOTE ALEX. This ref format does not appear to get picked up by vesta
+    https://open-babel.readthedocs.io/en/latest/FileFormats/Extended_XYZ_cartesian_coordinates_format.html
+
+    :param xyz_file:
+    :param atoms:
+    :return:
+    """
+    with open(xyz_file, "w") as f:
+        f.write(f"{len(atoms)}\n")
+        f.write("%PBC\n")
+
+        for symbol, position in zip(
+            atoms.get_chemical_symbols(), atoms.get_positions()
+        ):
+            f.write(
+                f"{symbol} {position[0]:.6f} {position[1]:.6f} {position[2]:.6f}\n"
+            )
+
+        f.write("\n")
+        lattice = atoms.get_cell().array
+        n_vectors, n_components = len(lattice), len(lattice[0])
+
+        for i in range(n_vectors):
+            f.write(
+                f"Vector{i + 1}   "
+                + " ".join(f"{r:.9f}" for r in lattice[i, :])
+                + "\n"
+            )
+
+        origin = np.zeros(shape=n_components)
+        f.write("Offset    " + " ".join(f"{r:.9f}" for r in origin))
